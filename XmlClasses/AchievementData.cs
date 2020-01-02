098e9de1a39a5b7e6820e6952783679e549b62f2
Nes Croft <nes.croft@gmail.com>
2020-01-02 12:09:46 -0800

    new pages

---
+++ XmlClasses/AchievementData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorServerArcane.XmlClasses {
+    public class AchievementData {
+        public string achievementName { get; set; }
+        public string actionType { get; set; }
+        public string achievementDescription { get; set; }
+        public string count { get; set; }
+        public string achievementUniqueID { get; set; }
+        public string itemType { get; set; }
+        public string itemCategory { get; set; }
+        public string iconPath { get; set; }
+        public string tier { get; set; }
+    }
+
+}
